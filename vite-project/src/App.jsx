eff4f1cf9dabd53f78cddd340af85585143e7550
Sakshi <sakshipathak3007@gmail.com>
2024-04-18 13:07:10 +0530

    Frontend setup ready

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { FunctionComponent } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
