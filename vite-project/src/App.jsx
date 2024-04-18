cd31a48fe8ecc95186593cb0ba5ef5da40f6e70d
Sakshi <sakshipathak3007@gmail.com>
2024-04-18 17:47:26 +0530

    packages installed

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-import { FunctionComponent } from 'react'
-
+import  Header  from './components/Header.jsx';
+import Home from './pages/Home.jsx'
 import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   
+         <BrowserRouter>
+         <Header />
+         <Routes>
+              <Route path="/" element={<Home/>}/>
+         </Routes>
+         </BrowserRouter>
     </>
   )
 }
