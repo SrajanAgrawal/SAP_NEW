115ed0980e1f78adc846a95694fa80f3cf16d667
Sakshi <sakshipathak3007@gmail.com>
2024-05-04 23:47:05 +0530

    background image and buttons add in hero section

--- vite-project/src/components/HeroSection.jsx
+++ vite-project/src/components/HeroSection.jsx
@@ -1,25 +1,31 @@
+import { Button } from "flowbite-react";
+import { HiOutlineArrowRight } from "react-icons/hi";
 
 
 const HeroSection = () => {
   return (
     <div>
       <div className='min-h-screen'>
-        <div className='flex mx-auto md:flex-row md:items-center hero-section  items-center justify-center'>
+        <div className='flex mx-auto md:flex-row md:items-center hero-section '>
           {/* left */}
-          <div className=' text-white w-[70%]' >
-            <h1 className='text-2xl self-center font-semibold '> STUDENT AMBASSDOR's PROGRAM</h1>
+          <div className=' text-white w-[45%] mx-10' >
+            <h1 className='text-2xl font-semibold '> STUDENT AMBASSDOR'S PROGRAM</h1>
             <p className='mt-10 text-lg'> A Student Ambassador Program fosters leadership among students, empowering them
               to represent their school or institution. These ambassadors serve as liaisons
               between students, faculty, and administration, facilitating communication and
               fostering a sense of community. Through various initiatives, they promote school
-              spirit, engagement, and involvement in extracurricular activities. They often assist
-              with campus tours, orientation programs, and recruitment efforts, showcasing the
-              institution's culture and values. Additionally, they provide support to new students,
-              helping them navigate academic and social aspects of campus life. Ultimately, the Student
-              Ambassador Program cultivates a vibrant and inclusive environment, enriching the overall
-              student experience.</p>
+              spirit, engagement, and involvement in extracurricular activities. </p>
+            <div className=" mt-5 flex flex-row ">
+              <Button outline gradientDuoTone="purpleToBlue">
+                Apply Now
+              </Button>
+
+              <Button className="mx-5">
+                See Benefits
+                <HiOutlineArrowRight className="ml-2 h-5 w-5 " />
+              </Button>
+            </div>
           </div>
-          
         </div>
       </div>
       <div className=''>
